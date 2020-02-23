65872d8619c9fafa10aec3a7162458918823a720
hedgehogus <liudmyla.denysiuk@itrexgroup.com>
2020-02-23 21:47:45 +0200

    8

---
+++ EventLoopEmulation.js
@@ -0,0 +1,36 @@
+// node myFile.js
+
+const pendingTimers = [];
+const pendingOSTasks = [];
+const pendingOperations = [];
+
+// New timers, tasks, operation are recorded from myFile running
+myFile.runContents();
+
+function shouldContinue() {
+    // Check one: Any pending setTimeout, setInterval, setImmediate?
+    // Check two: Any pending OS tasks? (like server listening to port)
+    // Check three: Any pending long running operations? (Like fs module)
+
+    return pendingTimesr.length || pendingOSTasks.length || pendingOperations.length;
+}
+
+// entire body executes in one 'tick'
+while (shouldContinue()) {
+    // 1) node looks at pendingTimers and sees if any function are ready to be called. setTimeout, setInterval
+
+    // 2) node looks at pendingOSTasks and pendingOperations and calls relevant callbacks
+
+    // 3) Pause execution. Continue when ...
+    //    - a new pendingOSTasks is done  
+    //    - a new pendingOperation is done
+    //    - a timer is about to complete
+
+    // 4) Look at pendingTimers. Call any setImmediate
+
+    // 5) Handle any 'close' events (cleanup code, like open files)
+}
+
+
+
+// exit back to terminal
