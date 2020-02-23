51a5c9a640ebf4e39a3b3a2b81e01117f0f86cee
hedgehogus <research162@gmail.com>
2020-02-23 23:40:42 +0200

    18

---
+++ threads.js
@@ -0,0 +1,20 @@
+process.env.UV_THREADPOOL_SIZE = 2; // not working for windows, need to set in package.json
+
+const crypto = require('crypto');
+
+const start = Date.now();
+crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+    console.log('1:', Date.now() - start);
+})
+crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+    console.log('2:', Date.now() - start);
+})
+crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+    console.log('3:', Date.now() - start);
+})
+crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+    console.log('4:', Date.now() - start);
+})
+crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+    console.log('5:', Date.now() - start);
+})
