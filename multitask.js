462ede14b22a609edf742715cf8f18c16730566a
hedgehogus <research162@gmail.com>
2020-02-25 01:29:48 +0200

    22

--- multitask.js
+++ multitask.js
@@ -1,4 +1,6 @@
 const https = require('https');
+const crypto = require('crypto');
+const fs = require('fs');
 
 const start = Date.now();
 
@@ -11,10 +13,21 @@ function doRequest() {
     }).end();
 }
 
+function doHash(index){
+    crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+        console.log(`${index}:`, Date.now() - start);
+    }) 
+}
+
 doRequest();
-doRequest(); 
-doRequest(); 
-doRequest();
-doRequest(); 
-doRequest(); 
-doRequest();  
+
+fs.readFile('multitask.js', 'utf8', () => {
+    console.log('FS:', Date.now() - start);
+})
+
+doHash(1);
+doHash(2);
+doHash(3);
+doHash(4);
+doHash(5);
+doHash(6);
