1b3b6af9916298613c642fae302259b7f40c834e
hedgehogus <research162@gmail.com>
2020-02-26 01:43:47 +0200

    28

--- improvingPerfomance/index.js
+++ improvingPerfomance/index.js
@@ -1,22 +1,31 @@
 const cluster = require('cluster');
+const crypto = require('crypto');
 
 // is the file being executed in master mode?
 if (cluster.isMaster){
     // Cause index.js to be executed *again* but in child(slave) mode
     cluster.fork();
+ //   cluster.fork();
+ //   cluster.fork();
+ //   cluster.fork();
 } else {
     // Im a child, Im going to act like a server and do nothing else
     const express = require('express');
     const app = express();
 
-    function doWork(duration){
+    /*function doWork(duration){
         const start = Date.now();
         while(Date.now() - start < duration){ }
-    }
+    }*/
 
     app.get('/', (req,res) => {
-        doWork(5000);
-        res.send('Hi there');
+        crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+            res.send('Hi there');
+        });
+    })
+
+    app.get('/fast', (req, res) => {
+        res.send('this was fast');
     })
     app.listen(4000);
 }
