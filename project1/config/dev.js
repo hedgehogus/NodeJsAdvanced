1ab57feb530a8ca122679d8ddd0de85db3562b78
hedgehogus <research162@gmail.com>
2020-02-29 01:03:01 +0200

    39

--- project1/config/dev.js
+++ project1/config/dev.js
@@ -2,6 +2,6 @@ module.exports = {
   googleClientID:
     '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
-  mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
+  mongoURI: 'mongodb://hedgehog:abh1abh2abh3@cluster0-shard-00-00-pdqb3.mongodb.net:27017,cluster0-shard-00-01-pdqb3.mongodb.net:27017,cluster0-shard-00-02-pdqb3.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority',
   cookieKey: '123123123'
 };
