a7540e4c7b0b5efadb80760a9d262fa29b74fb09
hedgehogus <research162@gmail.com>
2020-03-05 00:03:10 +0200

    56

--- project1/services/cache.js
+++ project1/services/cache.js
@@ -1,16 +1,16 @@
 const mongoose = require('mongoose');
+const redis = require('redis');
+const util = require('util');
 
+redisUrl = 'redis://127.0.0.1:6379';
+const client = redis.createClient(redisUrl);
 const exec = mongoose.Query.prototype.exec;
 
 mongoose.Query.prototype.exec = function () {
-    console.log('IM ABOUT TO RUN A QUERY');
 
     const key = Object.assign({}, this.getQuery, {
       collection: this.mongooseCollection.name  
     });
-
-    console.log(key);
-
 
     return exec.apply(this, arguments);
 }
