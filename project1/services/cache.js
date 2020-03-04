88c471e700be11a3a000c8b0407f4e94cf55972c
hedgehogus <research162@gmail.com>
2020-03-05 01:09:57 +0200

    57

--- project1/services/cache.js
+++ project1/services/cache.js
@@ -6,11 +6,22 @@ redisUrl = 'redis://127.0.0.1:6379';
 const client = redis.createClient(redisUrl);
 const exec = mongoose.Query.prototype.exec;
 
-mongoose.Query.prototype.exec = function () {
+mongoose.Query.prototype.exec = async function () {
 
-    const key = Object.assign({}, this.getQuery, {
+    const key = JSON.stringify(Object.assign({}, this.getQuery, {
       collection: this.mongooseCollection.name  
-    });
+    }));
 
-    return exec.apply(this, arguments);
+    // see if we have a value for 'key' in redis
+    const cacheValue = await client.get(key);
+
+    // if we do, return that
+    if (cacheValue) {
+        console.log(cacheValue);
+    }
+
+    // Otherwise, issue the query and store the result in redis
+
+    const result = await exec.apply(this, arguments);
+    console.log(result);
 }
