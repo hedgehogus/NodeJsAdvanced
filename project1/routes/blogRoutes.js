b1b2e1ee09e5b006377d9962b6c82a37b7e96b22
hedgehogus <research162@gmail.com>
2020-03-25 22:20:52 +0200

    65

--- project1/routes/blogRoutes.js
+++ project1/routes/blogRoutes.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
+const { clearHash } = require('../services/cache');
 
 const Blog = mongoose.model('Blog');
 
@@ -15,7 +16,7 @@ module.exports = app => {
 
   app.get('/api/blogs', requireLogin, async (req, res) => {    
     const blogs = await Blog
-      .find({ _user: req.user.id })
+      .find({ _user: req.user.id})
       .cache({ key: req.user.id});
     res.send(blogs);
   });
@@ -35,5 +36,9 @@ module.exports = app => {
     } catch (err) {
       res.send(400, err);
     }
+
+    //clearHash(req.user.id);
   });
 };
+
+
