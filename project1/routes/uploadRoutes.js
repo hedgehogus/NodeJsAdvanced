51ede73ecf47eb0006f3e4ee7e16556b09996aac
hedgehogus <research162@gmail.com>
2020-04-11 01:49:53 +0300

    152

--- project1/routes/uploadRoutes.js
+++ project1/routes/uploadRoutes.js
@@ -1,7 +1,15 @@
+const AWS = require('aws-sdk');
+const keys = require('../config/keys');
 
+const s3 = new AWS.S3({
+    accessKeyId: keys.accessKeyId,
+    secretAccessKey: keys.secretAccessKey
+})
+
+// npm install --save aws-sdk
 
 module.exports = app => {
     app.get('/api/upload', (req, res) => {
-        
+
     })
 };
