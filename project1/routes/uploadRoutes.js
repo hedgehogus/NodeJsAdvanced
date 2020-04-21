973bb20bf060720e59d9373908268197a3aee2c7
hedgehogus <research162@gmail.com>
2020-04-22 00:19:12 +0300

    155

--- project1/routes/uploadRoutes.js
+++ project1/routes/uploadRoutes.js
@@ -1,4 +1,6 @@
 const AWS = require('aws-sdk');
+const uuid = require('uuid/v1');
+const requierLogin = require('../middlewares/requireLogin');
 const keys = require('../config/keys');
 
 const s3 = new AWS.S3({
@@ -7,9 +9,16 @@ const s3 = new AWS.S3({
 })
 
 // npm install --save aws-sdk
+// npm install --save uuid
 
 module.exports = app => {
-    app.get('/api/upload', (req, res) => {
+    app.get('/api/upload', requierLogin, (req, res) => {
+        const key = `${req.user.id}/${uuid()}.jpeg`;
 
+        s3.getSignedUrl('putObject', {
+            Bucket: 'name-of-the-bucket',
+            ContentType: 'jpeg',
+            Key: key
+        }, (err, url) => res.send({ key, url }));
     })
 };
